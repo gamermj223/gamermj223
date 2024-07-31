8f84a9454f75faef62fccdbff08b4e118702c110
gamermj223 <youtmomgay123@gmail.com>
2024-07-31 16:35:28 -0400

    Update main.js

--- clickergame/main.js
+++ clickergame/main.js
@@ -1,6 +1,9 @@
-clicks = 0;
+let clicks = Number(localStorage.getItem('clicks')) || 0
+
+document.getElementById('clickerbutton').innerHTML = clicks;
 
 document.getElementById('clickerbutton').onclick = function(){
     clicks += 1;
     document.getElementById('clickerbutton').innerHTML = clicks;
+    localStorage.setItem('clicks', clicks);
 }
