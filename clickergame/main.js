90a4f120ae6e46a0a1a3fa66c334393643151bf6
gamermj223 <youtmomgay123@gmail.com>
2024-08-02 21:29:19 -0400

    Update main.js

--- clickergame/main.js
+++ clickergame/main.js
@@ -1,15 +1,32 @@
 let clicks = Number(localStorage.getItem('clicks')) || 0
+let ClickerUpgradeLevel = Number(localStorage.getItem('UpgradeNumber')) || 1
+let ClickerUpgradeCost = Number(localStorage.getItem('UpgradePercent')) || 100
 
 document.getElementById('clickerbutton').innerHTML = clicks;
+document.getElementById('clickerupgradebutton').innerHTML = 'Clicker Upgrade ' + ClickerUpgradeLevel + '<br>' + 'Cost - ' + ClickerUpgradeCost;
+
 
 document.getElementById('clickerbutton').onclick = function(){
-    clicks += 1;
-    document.getElementById('clickerbutton').innerHTML = clicks;
+    clicks += 1
+    document.getElementById('clickerbutton').innerHTML = clicks
     localStorage.setItem('clicks', clicks);
 }
 
+document.getElementById('giveclicks').onclick = function(){
+    clicks += 10000
+}
+
 document.getElementById('resetbutton').onclick = function(){
-    clicks = 0;
-    document.getElementById('clickerbutton').innerHTML = clicks;
-    localStorage.setItem('clicks', clicks);
+    ClickerUpgradeCost = 100
+    ClickerUpgradeLevel = 1
+    clicks = 0
+    document.getElementById('clickerbutton').innerHTML = clicks
+    localStorage.setItem('clicks', clicks)
+}
+
+document.getElementById('clickerupgradebutton').onclick = function(){
+    if (clicks <= 1000) {
+        ClickerUpgradeCost *= 2
+    }
+    document.getElementById('clickerupgradebutton').innerHTML = 'Clicker Upgrade ' + ClickerUpgradeLevel + '<br>' + 'Cost - ' + ClickerUpgradeCost;
 }
